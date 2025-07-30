9fdaa850a591af62dc7b246465ff200cfc0f3e58
Mugisha-isaac <123@Mugisha12!>
2025-07-30 18:07:48 +0200

    feat: Update application ID and package name in configuration files

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -9,7 +9,7 @@ plugins {
 }
 
 android {
-    namespace = "com.alu.jenga"
+    namespace = "com.example.jenga_app"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = "27.0.12077973"
 
@@ -24,7 +24,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.alu.jenga"
+        applicationId = "com.example.jenga_app"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = 23  // Updated to support Firebase Auth
